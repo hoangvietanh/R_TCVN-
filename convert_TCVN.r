7a801d58edc2cd8d4b0dc8a5909a162ab2eb279b
hoangvietanh <hoangvietanh@gmail.com>
2014-10-07 21:07:58 +0700

    Create convert_TCVN.r

---
+++ convert_TCVN.r
@@ -0,0 +1,5 @@
+require("foreign")
+setwd("c:/todel/uconv")
+map=read.dbf(file="DARON_RUNGKK.DAT",as.is = TRUE,)
+write.csv(map,file="in.csv")
+system("uvconv in.csv -f TCVN3 -t UTF-8 -o out.csv",intern = TRUE, ignore.stderr = TRUE)
